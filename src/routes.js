0d307090516bb1f1fc1f7956d777c170d2a60044
Edilson Lima <edilson.lima@nextem.com.br>
2020-05-09 23:16:36 -0400

    #1 Criação de rota para listagem de CTOs

--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,13 @@ import { Router } from 'express';
 
 import SessionController from './app/controllers/SessionController';
 import RequestController from './app/controllers/RequestController';
+import CTOController from './app/controllers/CTOController';
 
 const routes = new Router();
 
 routes.post('/sessions', SessionController.store);
 routes.post('/requests', RequestController.show);
 routes.post('/requests/status', RequestController.update);
+routes.get('/cto', CTOController.index);
 
 export default routes;
