df298c7ff8542b92c04393ddde2bbad639581b1a
Edilson Lima <edilson.lima@nextem.com.br>
2020-06-19 13:17:45 -0400

    #1 Liberação do update de clientes

--- src/app/controllers/ClientController.js
+++ src/app/controllers/ClientController.js
@@ -35,9 +35,8 @@ class ClientController {
     if (latitude && longitude) {
       client.coordenadas = `${latitude},${longitude}`;
     }
-    // await client.save();
+    await client.save();
     
-    return res.json(client);
     return res.sendStatus(200);
   }
 }
