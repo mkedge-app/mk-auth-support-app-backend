cffd0a7a024b158eeeeceb890165cdb5cbc76ebf
Edilson Lima <edilson.lima@nextem.com.br>
2020-06-19 15:57:42 -0400

    #1 Implementação do uso do middleware de autenticação para rotas seguras

--- src/routes.js
+++ src/routes.js
@@ -5,9 +5,13 @@ import RequestController from './app/controllers/RequestController';
 import CTOController from './app/controllers/CTOController';
 import ClientController from './app/controllers/ClientController';
 
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
 routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
 
 routes.post('/requests', RequestController.show);
 routes.post('/requests/status', RequestController.update);
