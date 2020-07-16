6757769b1e8da6be18f2af7b6580c2eb8efaf18c
Edilson Lima <edilsonlm217@hotmail.com>
2020-07-16 14:52:59 -0400

    #2 Criação de rota para expor métodos do SearchController

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import RequestController from './app/controllers/RequestController';
 import CTOController from './app/controllers/CTOController';
 import ClientController from './app/controllers/ClientController';
 import EmployeeController from './app/controllers/EmployeeController';
+import SearchController from './app/controllers/SearchController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -26,5 +27,7 @@ routes.get('/cto/:latitude/:longitude', CTOController.index);
 routes.get('/cto/:cto_name', CTOController.show);
 
 routes.get('/employees', EmployeeController.index);
+
+routes.get('/search', SearchController.index);
 
 export default routes;
