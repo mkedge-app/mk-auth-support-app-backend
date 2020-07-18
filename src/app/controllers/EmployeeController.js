68d70d3b23cfbc1184baec1bcb31575c9e6c5016
Edilson Lima <edilsonlm217@hotmail.com>
2020-07-18 00:43:59 -0400

    #2 Implementação do método show

--- src/app/controllers/EmployeeController.js
+++ src/app/controllers/EmployeeController.js
@@ -6,6 +6,14 @@ class EmployeeController {
 
     return res.json(employees);
   }
+
+  async show(req, res) {
+    const { id: employee_id } = req.params;
+
+    const employee = await Employee.findByPk(employee_id);
+
+    return res.json(employee);
+  }
 }
 
 export default new EmployeeController();
