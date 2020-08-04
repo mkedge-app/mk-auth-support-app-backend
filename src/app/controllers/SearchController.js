e511efd6000a5bf87e036c74d5216e55d2ec2e74
Edilson Lima <edilsonlm217@hotmail.com>
2020-08-04 12:28:04 -0400

    #3 Implementação para identificação do equipment status de cada resultado da busca

--- src/app/controllers/SearchController.js
+++ src/app/controllers/SearchController.js
@@ -1,7 +1,11 @@
 import { Op } from 'sequelize';
+import { endOfYear } from 'date-fns';
+
 import Client from '../models/Client';
+import Radacct from '../models/Radacct';
 
 class SearchController {
+  // eslint-disable-next-line consistent-return
   async index(req, res) {
     const { term } = req.query;
 
@@ -17,10 +21,48 @@ class SearchController {
             [Op.like]: `%${term}%`,
           },
         },
-        attributes: ['id', 'nome'],
+        attributes: ['id', 'nome', 'login'],
       });
 
-      return res.json(clients);
+      const promises = clients.map(client => {
+        return Radacct.findAll({
+          where: {
+            username: client.login,
+            acctstarttime: {
+              [Op.lte]: endOfYear(new Date()),
+            },
+          },
+          limit: 1,
+          order: [['acctstarttime', 'DESC']],
+          attributes: ['acctstoptime'],
+        });
+      });
+
+      Promise.all(promises).then(response => {
+        const equipment_array = [];
+        // eslint-disable-next-line array-callback-return
+        response.map(item => {
+          if (item.length !== 0) {
+            let equipment_status = 'Offline';
+            equipment_status =
+              item[0].acctstoptime === null ? 'Online' : 'Offline';
+
+            equipment_array.push(equipment_status);
+          } else {
+            const equipment_status = 'Offline';
+            equipment_array.push(equipment_status);
+          }
+        });
+
+        clients.forEach((client, index) => {
+          clients[index] = {
+            ...client.dataValues,
+            equipment_array: equipment_array[index],
+          };
+        });
+
+        return res.json(clients);
+      });
     }
 
     const clients = await Client.findAll({
@@ -29,10 +71,48 @@ class SearchController {
           [Op.like]: `%${term}%`,
         },
       },
-      attributes: ['id', 'nome'],
+      attributes: ['id', 'nome', 'login'],
     });
 
-    return res.json(clients);
+    const promises = clients.map(client => {
+      return Radacct.findAll({
+        where: {
+          username: client.login,
+          acctstarttime: {
+            [Op.lte]: endOfYear(new Date()),
+          },
+        },
+        limit: 1,
+        order: [['acctstarttime', 'DESC']],
+        attributes: ['acctstoptime'],
+      });
+    });
+
+    Promise.all(promises).then(response => {
+      const equipment_array = [];
+      // eslint-disable-next-line array-callback-return
+      response.map(item => {
+        if (item.length !== 0) {
+          let equipment_status = 'Offline';
+          equipment_status =
+            item[0].acctstoptime === null ? 'Online' : 'Offline';
+
+          equipment_array.push(equipment_status);
+        } else {
+          const equipment_status = 'Offline';
+          equipment_array.push(equipment_status);
+        }
+      });
+
+      clients.forEach((client, index) => {
+        clients[index] = {
+          ...client.dataValues,
+          equipment_array: equipment_array[index],
+        };
+      });
+
+      return res.json(clients);
+    });
   }
 }
 
