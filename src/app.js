1f5341225aa688081360b49b58432bbb0776ea51
Edilson Lima <edilsonlm217@hotmail.com>
2020-08-20 10:35:17 -0400

    #6 Criação de conexão com MongoDB

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import mongoose from 'mongoose';
 import routes from './routes';
 
 import './database';
@@ -12,6 +13,7 @@ class App {
 
     this.middlewares();
     this.routes();
+    this.mongo();
   }
 
   middlewares() {
@@ -21,6 +23,17 @@ class App {
 
   routes() {
     this.server.use(routes);
+  }
+
+  mongo() {
+    this.mongoConnection = mongoose.connect(
+      'mongodb+srv://rest-api-mk-edge:mk-edge-2020@cluster0.dmaf2.mongodb.net/mk-edge?retryWrites=true&w=majority',
+      {
+        useNewUrlParser: true,
+        useFindAndModify: true,
+        useUnifiedTopology: true,
+      }
+    );
   }
 
   initDataBaseObserver() {
