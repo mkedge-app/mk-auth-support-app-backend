9d96550b2a49051be701bdc4344959cd404e88cb
Edilson Lima <edilsonlm217@hotmail.com>
2020-08-22 11:46:26 -0400

    #6 Inclusão do atributo header

--- src/app/schemas/notification.js
+++ src/app/schemas/notification.js
@@ -2,6 +2,10 @@ import mongoose from 'mongoose';
 
 const NotificationSchema = new mongoose.Schema(
   {
+    header: {
+      type: String,
+      required: true,
+    },
     content: {
       type: String,
       required: true,
