06231eb9d61e7bc45eb103dd54c144c2b7a27d79
Edilson Lima <edilsonlm217@hotmail.com>
2020-08-25 09:17:00 -0400

    #6 Implementação para marcar todas as notificações como visualizadas

--- src/app/controllers/NotificationController.js
+++ src/app/controllers/NotificationController.js
@@ -1,14 +1,35 @@
 import Notification from '../schemas/notification';
 
 class NotificationController {
+  async update(req, res) {
+    const { employee_id, viewedAt } = req.body;
+    const not_viewed_notifications = await Notification.find()
+      .where('viewed')
+      .equals(false)
+      .where('user')
+      .equals(employee_id);
+
+    not_viewed_notifications.forEach(async element => {
+      const notification = await Notification.findById(element.id);
+
+      if (notification) {
+        notification.viewed = true;
+        notification.viewedAt = viewedAt;
+        notification.save();
+      }
+    });
+
+    return res.json({ ok: true });
+  }
+
   async show(req, res) {
     const { employee_id } = req.params;
 
-    const notificatios = await Notification.find({
+    const notifications = await Notification.find({
       user: employee_id,
     });
 
-    return res.json({ notificatios });
+    return res.json({ notifications });
   }
 }
 
