2bc21c0489587ba41055af1d7a635e71ee1f2b5f
Edilson Lima <edilsonlm217@hotmail.com>
2020-08-28 14:59:02 -0400

    #6 Implementação para Envio da push notification

--- src/observers/DatabaseObserver.js
+++ src/observers/DatabaseObserver.js
@@ -1,6 +1,8 @@
 import NotificationSending from '../app/jobs/NotificationSending';
+
 import Queue from '../lib/queue';
 import SocketIO from '../lib/socket';
+import PushNotificationSender from '../lib/oneSignal';
 
 import Notification from '../app/schemas/notification';
 
@@ -16,13 +18,23 @@ class DatabaseObserver {
     // eslint-disable-next-line no-underscore-dangle
     const connectedUsers = SocketIO.users;
 
-    const socketOwner = connectedUsers[employee_id];
+    const socketOwner = connectedUsers[employee_id].socketId;
+    const { oneSignalUserId } = connectedUsers[employee_id];
 
     if (socketOwner) {
       await Queue.add(NotificationSending.key, {
         notification,
         socketOwner,
       });
+
+      // Envio da push notification
+      const pushNotificationMessage = {
+        app_id: 'ba39cc13-1ac4-46f6-82f5-929b5b3a6562',
+        contents: { en: 'English Message' },
+        include_player_ids: [oneSignalUserId],
+      };
+
+      PushNotificationSender.pushNotification(pushNotificationMessage);
     }
   }
 }
