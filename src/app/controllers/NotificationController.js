7ad64e37bfe26d27c5c58d0bd3bcc22a663e041d
Edilson Lima <edilsonlm217@hotmail.com>
2020-09-02 11:23:55 -0400

    #6 Adaptação para consumo de notificações a a partir do banco relacional

--- src/app/controllers/NotificationController.js
+++ src/app/controllers/NotificationController.js
@@ -1,4 +1,4 @@
-import Notification from '../schemas/notification';
+import Notification from '../models/Notification';
 
 class NotificationController {
   async update(req, res) {
@@ -6,33 +6,36 @@ class NotificationController {
 
     switch (action) {
       case 'markAsViewed': {
-        const { viewedAt } = req.body;
-        const not_viewed_notifications = await Notification.find()
-          .where('user')
-          .equals(employee_id)
-          .sort({ createdAt: 'desc' });
+        const { viewed_at } = req.body;
+
+        const not_viewed_notifications = await Notification.findAll({
+          where: {
+            user: employee_id,
+          },
+          order: [['created_at', 'DESC']],
+        });
 
         if (!not_viewed_notifications) {
-          return res.status(400).json({ message: 'No notification not found' });
+          return res.status(400).json({ message: 'No notification found' });
         }
 
         const response = [];
         not_viewed_notifications.forEach(async element => {
-          if (element.viewed === false) {
-            element.viewed = true;
-            element.viewedAt = viewedAt;
+          if (element.is_viewed === false) {
+            element.is_viewed = true;
+            element.viewed_at = viewed_at;
           }
 
           response.push(element);
         });
 
         not_viewed_notifications.forEach(async element => {
-          const notification = await Notification.findById(element.id);
+          const notification = await Notification.findByPk(element.id);
 
-          if (notification.viewed === false) {
+          if (notification.is_viewed === false) {
             if (notification) {
-              notification.viewed = true;
-              notification.viewedAt = viewedAt;
+              notification.is_viewed = true;
+              notification.viewed_at = viewed_at;
               await notification.save();
             }
           }
@@ -44,9 +47,9 @@ class NotificationController {
       case 'markAsRead': {
         const { notification_id } = req.body;
 
-        const notification = await Notification.findById(notification_id);
+        const notification = await Notification.findByPk(notification_id);
         if (notification) {
-          notification.isRead = true;
+          notification.is_read = true;
           await notification.save();
         }
 
@@ -57,19 +60,20 @@ class NotificationController {
         break;
     }
 
-    const notifications = await Notification.find({
-      user: employee_id,
-    }).sort({ createdAt: 'desc' });
-
-    return res.json({ notifications });
+    return res.json({ ok: true });
   }
 
   async show(req, res) {
-    const { employee_id } = req.params;
+    // eslint-disable-next-line radix
+    const employee_id = parseInt(req.params.employee_id);
 
-    const notifications = await Notification.find({
-      user: employee_id,
-    }).sort({ createdAt: 'desc' });
+    const notifications = await Notification.findAll({
+      where: {
+        // eslint-disable-next-line radix
+        user: employee_id,
+      },
+      order: [['created_at', 'DESC']],
+    });
 
     return res.json({ notifications });
   }
