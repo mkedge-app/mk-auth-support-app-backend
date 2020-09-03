abce4eb0f35ae5ae07cc44b75a0a20a38c49ec1e
Edilson Lima <edilsonlm217@hotmail.com>
2020-09-03 10:13:02 -0400

    #6 Criação do método addNewUser

--- src/lib/oneSignal.js
+++ src/lib/oneSignal.js
@@ -2,7 +2,25 @@
 import https from 'https';
 
 class PushNotificationSender {
-  pushNotification(message) {
+  constructor() {
+    this.connectedUsers = {};
+  }
+
+  addNewUser(data) {
+    const { employee_id, oneSignalUserId } = data;
+
+    if (this.connectedUsers[employee_id] !== oneSignalUserId) {
+      this.connectedUsers[employee_id] = oneSignalUserId;
+    }
+  }
+
+  pushNotification(message, employee_id) {
+    const pushMessage = {
+      app_id: 'ba39cc13-1ac4-46f6-82f5-929b5b3a6562',
+      contents: { en: message },
+      include_player_ids: [this.connectedUsers[employee_id]],
+    };
+
     const headers = {
       'Content-Type': 'application/json; charset=utf-8',
     };
@@ -27,7 +45,7 @@ class PushNotificationSender {
       console.log(e);
     });
 
-    req.write(JSON.stringify(message));
+    req.write(JSON.stringify(pushMessage));
     req.end();
   }
 }
