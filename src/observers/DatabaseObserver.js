20628289e45d38c253829762d3bb8b0adb50420b
Edilson Lima <edilsonlm217@hotmail.com>
2020-09-03 10:13:52 -0400

    #6 Implementação para tornar notificação push independente do envio de msg pelo websocket

--- src/observers/DatabaseObserver.js
+++ src/observers/DatabaseObserver.js
@@ -21,21 +21,14 @@ class DatabaseObserver {
 
       if (connectedUsers[employee_id]) {
         const socketOwner = connectedUsers[employee_id].socketId;
-        const { oneSignalUserId } = connectedUsers[employee_id];
 
         await Queue.add(NotificationSending.key, {
           socketOwner,
         });
-
-        // Envio da push notification
-        const pushNotificationMessage = {
-          app_id: 'ba39cc13-1ac4-46f6-82f5-929b5b3a6562',
-          contents: { en: message },
-          include_player_ids: [oneSignalUserId],
-        };
-
-        PushNotificationSender.pushNotification(pushNotificationMessage);
       }
+
+      // Envio da push notification
+      PushNotificationSender.pushNotification(message, employee_id);
     }
   }
 }
