c62e439c038041e09d26b7788d26ef497801bd86
Edilson Lima <edilsonlm217@hotmail.com>
2020-10-15 14:47:03 -0400

    #8 Remoção da conexão direta com o mkradius database

--- src/database/index.js
+++ src/database/index.js
@@ -1,40 +1,15 @@
-import Sequelize from 'sequelize';
-
-import User from '../app/models/User';
-import Request from '../app/models/Request';
-import Employee from '../app/models/Employee';
-import Client from '../app/models/Client';
-import Mensagem from '../app/models/Mensagem';
-import SystemLog from '../app/models/SystemLog';
-import CTO from '../app/models/CTO';
-import Radacct from '../app/models/Radacct';
-import Invoice from '../app/models/Invoice';
-import Notification from '../app/models/Notification';
-
-import databaseConfig from '../config/database';
-
-const models = [
-  User,
-  Request,
-  Employee,
-  Client,
-  Mensagem,
-  SystemLog,
-  CTO,
-  Radacct,
-  Invoice,
-  Notification,
-];
+import mongoose from 'mongoose';
 
 class Database {
   constructor() {
-    this.init();
+    this.mongo();
   }
 
-  init() {
-    this.connection = new Sequelize(databaseConfig);
-
-    models.map(model => model.init(this.connection));
+  mongo() {
+    this.mongoConnection = mongoose.connect(
+      'mongodb://192.168.99.100:27017/mkedgetenants',
+      { useNewUrlParser: true, useUnifiedTopology: true }
+    );
   }
 }
 
