ee6fe987b35bd9b3a27a8bd6b90e24efdebdd676
Edilson Lima <edilsonlm217@hotmail.com>
2020-12-28 22:32:10 -0400

    #10 Alteração do tipo do atributo fechamento

--- src/app/models/SupportRequest.js
+++ src/app/models/SupportRequest.js
@@ -15,7 +15,7 @@ class SupportRequest extends Model {
         assunto: Sequelize.STRING,
         visita: Sequelize.DATE,
         chamado: Sequelize.STRING,
-        fechamento: Sequelize.STRING,
+        fechamento: Sequelize.DATE,
         motivo_fechar: Sequelize.STRING,
         prioridade: Sequelize.STRING,
       },
