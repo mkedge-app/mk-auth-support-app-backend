e0e340dc6bccc195dd9159a4b7ec2ea7f0d6e082
Edilson Lima <edilsonlm217@hotmail.com>
2021-01-05 18:02:45 -0400

    #10 Conexão com banco de dados de forma autenticada

--- src/database/index.js
+++ src/database/index.js
@@ -6,8 +6,15 @@ class Database {
   }
 
   mongo() {
+    // Conexão com o mongo modo Desenvolvimento
+    // this.mongoConnection = mongoose.connect(
+    //   'mongodb://192.168.99.100:27017/mkedgetenants',
+    //   { useNewUrlParser: true, useUnifiedTopology: true }
+    // );
+
+    // Conexão com mongo modo Produção
     this.mongoConnection = mongoose.connect(
-      'mongodb://localhost:27017/mkedgetenants',
+      'mongodb://root:F@lcon31@localhost:27017/mkedgetenants',
       { useNewUrlParser: true, useUnifiedTopology: true }
     );
   }
