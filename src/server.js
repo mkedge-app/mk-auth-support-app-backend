5f24d4742205988d5ee27def2727b6a0780ccee0
Willian Silva <silva.williansantos@gmail.com>
2021-02-05 17:20:32 -0300

    Add address application

--- src/server.js
+++ src/server.js
@@ -1,6 +1,12 @@
+import ip from 'ip';
 import app from './app';
 import logger from './logger';
 import appConfig from './config/app';
 
-logger.info(`This application is listening at the port: ${appConfig.app_port}`);
+logger.info(
+  `This is internal URL of the application: ${ip.address()}:${
+    appConfig.app_port
+  }`
+);
+
 app.listen(appConfig.app_port);
