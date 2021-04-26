3c1769eafbf53ee16a1fd4b823fa0a09cf915576
Edilson Lima <edilsonlm217@hotmail.com>
2021-04-26 00:01:06 -0400

    Agrupamento de permissões em um objeto

--- src/app/middlewares/permission.js
+++ src/app/middlewares/permission.js
@@ -1,14 +1,34 @@
 import User from '../models/User';
 import Permissions from '../models/Permissions';
 
+const permissions = {
+  update_employee: 'perm_chamAlterar',
+  close_request: 'perm_chamFechar',
+  update_visita_time: 'perm_chamAlterar',
+  update_visita_date: 'perm_chamAlterar',
+  add_note: 'perm_chamResponder',
+  update_client: 'perm_altcliente',
+  update_client_location: 'perm_altcliente',
+  // action: permission
+};
+
 export default async (req, res, next) => {
+  const { action } = req.body;
+
+  if (!action) {
+    return res.status(400).json({
+      message: 'Requisição inválida. Ação não informada!',
+      code: 400,
+    });
+  }
+
   try {
     const { login: userLogin } = await User.findByPk(req.idacesso);
 
     const permission = await Permissions.findOne({
       where: {
         usuario: userLogin,
-        nome: req.body.permission,
+        nome: permissions[action],
       },
     });
 
