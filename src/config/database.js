45ae685a6b2b62acd6556614942d7cd344229380
Edilson Lima <edilsonlm217@hotmail.com>
2023-02-05 14:27:08 -0400

    configuração db

--- src/config/database.js
+++ src/config/database.js
@@ -1,5 +1,5 @@
 const mongoUrl =
-  'mongodb://root:Falcon2931@mk-edge.com.br:27017/mkedgetenants?authSource=admin&authMechanism=SCRAM-SHA-256&readPreference=primary&appname=MongoDB%20Compass&ssl=false';
+  'mongodb://root:Falcon2931@localhost:27017/mkedgetenants?authSource=admin&authMechanism=SCRAM-SHA-256&readPreference=primary&appname=MongoDB%20Compass&ssl=false';
 
 export default {
   mongodb_auth_source: 'admin',
